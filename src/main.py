d21ac1e306dccdf44a83312ae5f58a2e85d6d0a0
KUCIA <noiszewczyk@gmail.com>
2026-03-28 17:59:45 +0100

    feat: add a callback on voice messages to set a reaction and reply with random phrases from time to time

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 """Main file."""
 
 import logging
+import random
 from os import environ
+from pathlib import Path
 
 from dotenv import load_dotenv
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.constants import ReactionEmoji
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 load_dotenv("env/.env")
 
@@ -26,7 +29,6 @@ async def start(update: Update) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
-
 async def help_command(update: Update) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
@@ -35,6 +37,22 @@ async def help_command(update: Update) -> None:
 async def echo(update: Update) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
+
+async def reply_to_fart_voice(update: Update, _: CallbackContext) -> None:
+    """Reply to the fart voice message with a random phrase with 20% chance and set a poop reaction."""
+    logger.info("Received voice message")
+
+    # Set the poop reaction on the message
+    await update.message.set_reaction(ReactionEmoji.PILE_OF_POO)
+
+    # 80% chance to ignore
+    if random.random() > 0.2:
+        return
+
+    # Reply with a random phrase from the list
+    path = Path(__file__).parent.parent / "resources" / "fart_reactions.txt"
+    phrase = random.choice(path.read_text().splitlines())
+    await update.message.reply_text(phrase, reply_to_message_id=update.message.message_id)
 
 
 def main() -> None:
@@ -48,6 +66,9 @@ def main() -> None:
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Voice message
+    application.add_handler(MessageHandler(filters.VOICE, callback=reply_to_fart_voice))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
